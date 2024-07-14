e8d7c6ed0beec8fbe3e243b566b1ea571c36b13b
lunaxislu <cmk0905@naver.com>
2024-07-14 19:39:34 +0900

    [mixed content] 5차시도 통합작업

--- src/pages/api/mixedError/mixedContent.ts
+++ src/pages/api/mixedError/mixedContent.ts
@@ -1,17 +1,9 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// const animalHospitalAPI = axios.create({
-//   baseURL: `${process.env.NEXT_PUBLIC_ANIMAL_HOSPITAL}`,
-// });
-
-// const animalPharamcyAPI = axios.create({
-//   baseURL: `${process.env.NEXT_PUBLIC_ANIMAL_PHARAMCY}`,
-// });
-const animalHospitalAPI = axios.create({});
-
-const animalPharamcyAPI = axios.create({});
-
+const animalMedicineAPI = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_ANIMAL_MEDICINE}`,
+});
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -19,14 +11,10 @@ export default async function handler(
   const { api_type, query_type, area, endRange } = req.body;
   console.log("🚀 ~ body:", req.body);
 
-  const api_fn = {
-    animal_hospital: animalHospitalAPI,
-    animal_pharmacy: animalPharamcyAPI,
-  };
   try {
-    const result = await api_fn[
-      api_type as "animal_hospital" | "animal_pharmacy"
-    ](`${query_type}${area}/1/${endRange}/01`);
+    const result = await animalMedicineAPI(
+      `${query_type}${area}/1/${endRange}/01`,
+    );
     console.log(result);
     res.status(200).send({ data: result.data, query_string: query_type, area });
   } catch (err) {
