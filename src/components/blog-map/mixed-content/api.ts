3df4d17b5a3681bfd36bcb55f694a72cab6749e6
lunaxislu <cmk0905@naver.com>
2024-07-16 01:17:28 +0900

    [mixed content] mapping 안해줘도 될까

--- src/components/blog-map/mixed-content/api.ts
+++ src/components/blog-map/mixed-content/api.ts
@@ -32,3 +32,30 @@ export const ParalledQueriesAnimalMedicineAPI = async (
     return []; // 성공 실패시 균일하게 해주기 위해서
   }
 };
+// process.env.NEXT_PUBLIC_SEOUL_API_URL은 api router 주소입니다.
+export const hasMixedContentParalledQueries = async (
+  area: string | null,
+  endRange: string,
+) => {
+  try {
+    const results = await Promise.all(
+      DYNAMIC_API_QURIES.map(async (query) => {
+        const result = await axios.post(
+          `/api/mixedError/hasMixedContent`,
+
+          {
+            area,
+            query_type: query.query_type, // 동물병원 || 동물약국 query
+            endRange,
+          },
+        );
+        return result.data;
+      }),
+    );
+
+    return results;
+  } catch (err) {
+    console.log(err, "map Error");
+    return []; // 성공 실패시 균일하게 해주기 위해서
+  }
+};
---
+++ src/pages/hasMixedContent.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import { Fragment, useState } from "react";
+
+import {
+  hasMixedContentParalledQueries,
+  ParalledQueriesAnimalMedicineAPI,
+} from "@/components/blog-map/mixed-content/api";
+import {
+  SEOUL_LOCATION,
+  SEOUL_QUERY,
+} from "@/components/blog-map/mixed-content/const";
+import { useQuery } from "@tanstack/react-query";
+import { refineSeoulApiData } from "../components/blog-map/mixed-content/util";
+const HasMixedContent = ({ endRange }: { endRange: string }) => {
+  const [query, setQuery] = useState<string | null>(null);
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: [SEOUL_QUERY.MEDICINE, query],
+    queryFn: () => hasMixedContentParalledQueries(query, "50"),
+    enabled: !!query,
+    // enabled: !!api_query && api_type === 'hospital',
+    select: refineSeoulApiData,
+    // refetchOnWindowFocus: false,
+    // staleTime: Infinity,
+  });
+
+  return (
+    <div style={{ display: "flex", gap: "20px", flexDirection: "column" }}>
+      <div style={{ display: "flex", gap: "20px" }}>
+        {SEOUL_LOCATION.map((Area) => (
+          <span
+            onClick={() => setQuery(Area.api_query)}
+            key={Area.location}
+            style={{
+              width: "80px",
+              backgroundColor: "wheat",
+              color: "black",
+              height: "80px",
+              textAlign: "center",
+            }}
+          >
+            {Area.location}
+          </span>
+        ))}
+      </div>
+      <div style={{ alignSelf: "center", fontSize: "24px" }}>
+        데이터가 mixed content없이 불러와진{" "}
+        <span
+          style={{
+            textDecoration: "underline",
+            color: "bisque",
+            fontSize: "32px",
+          }}
+        >
+          {isLoading ? "로딩중..." : data?.length + "개"}
+        </span>{" "}
+      </div>
+    </div>
+  );
+};
+
+export default HasMixedContent;
